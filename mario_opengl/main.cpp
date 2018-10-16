c8a2cd3fd84cac9d4c88bd906da67e2c5fcf4f37
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-10-16 16:35:05 +0300

    two triangles using one draw array call

--- mario_opengl/main.cpp
+++ mario_opengl/main.cpp
@@ -27,28 +27,20 @@ int main()
 	 0.5f,  0.5f, 0.0f,  // Верхний правый угол
 	 0.5f, -0.5f, 0.0f,  // Нижний правый угол
 	-0.5f, -0.5f, 0.0f,  // Нижний левый угол
-	-0.5f,  0.5f, 0.0f   // Верхний левый угол
+	-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+	-0.5f,  0.5f, 0.0f,   // Верхний левый угол
+	0.5f,  0.5f, 0.0f,  // Верхний правый угол
 	};
 
-	GLuint indices[] = {  // Помните, что мы начинаем с 0!
-	0, 1, 3,   // Первый треугольник
-	1, 2, 3    // Второй треугольник
-	};
-
-	GLuint ebo_id, vao_id, vbo_id;
+	GLuint vao_id, vbo_id;
 	glGenVertexArrays(1, &vao_id);
 	glGenBuffers(1, &vbo_id);
-	glGenBuffers(1, &ebo_id);
 
 	// start configure whole VAO
 	glBindVertexArray(vao_id);
 		
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_id);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
@@ -94,7 +86,7 @@ int main()
 
 		glUseProgram(shader_program_id);
 		glBindVertexArray(vao_id);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
@@ -102,7 +94,6 @@ int main()
 
 	glDeleteVertexArrays(1, &vao_id);
 	glDeleteBuffers(1, &vbo_id);
-	glDeleteBuffers(1, &ebo_id);
 	glfwTerminate();
 		
 	return 0;
